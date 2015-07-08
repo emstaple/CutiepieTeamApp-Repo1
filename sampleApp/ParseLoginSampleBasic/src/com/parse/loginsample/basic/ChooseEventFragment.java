0ecff14ce42ee92f8557946e8f5434906560d6b6
Elijah Staple <emstaple@fb.com>
2015-07-08 14:51:29 -0700

    Test

--- sampleApp/ParseLoginSampleBasic/src/com/parse/loginsample/basic/ChooseEventFragment.java
+++ sampleApp/ParseLoginSampleBasic/src/com/parse/loginsample/basic/ChooseEventFragment.java
@@ -1,11 +1,7 @@
 package com.parse.loginsample.basic;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.parse.ParseUser;
